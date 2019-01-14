d90c73a52aeb9a8c193776e2c0b6e6e79fb23aa4
kthiswell <kthiswell@rocode.co.kr>
2019-01-14 11:42:07 +0900

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('user connected');
+  socket.on('fromClient', function (data) {
+    console.log(data);
+  socket.broadcast.emit('fromServer', data);
+  });
+
+
+
+});
+
+
+server.listen(1127, function () {
+  console.log('listening on 1127 server');
+});
